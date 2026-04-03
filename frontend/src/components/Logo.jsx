7bd54a2e15ff57678d5074c0a297d978e51c45b7
anwenchao <ansusu1025@gmail.com>
2026-04-03 18:06:45 +0800

    fix bug

--- frontend/src/components/Logo.jsx
+++ frontend/src/components/Logo.jsx
@@ -3,7 +3,7 @@ import { cn } from "@/lib/utils";
 export default function Logo({ className }) {
   return (
     <img
-      src="/logo-mark.png"
+      src="/logo-mark-v2.png"
       alt="TechSpar"
       className={cn("shrink-0 block object-contain", className)}
     />
